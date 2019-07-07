fcdef254d15b3f3493a3b5a23b5013384980f1f3
ravibp <ravi.prasanna@infosys.com>
2019-07-07 18:33:11 +0530

    Skills Section half complete, styling pending.

--- src/myComponents/Header/Header.jsx
+++ src/myComponents/Header/Header.jsx
@@ -12,10 +12,11 @@ import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
 // @material-ui/icons
-import Menu from "@material-ui/icons/Menu";
 // core components
 import headerStyle from "assets/jss/material-kit-react/components/headerStyle.jsx";
 import "./Header.scss";
+import { red } from "@material-ui/core/colors";
+import HeaderLinks from "./HeaderLinks.jsx";
 
 class Header extends React.Component {
   constructor(props) {
@@ -28,105 +29,32 @@ class Header extends React.Component {
     this.setState({ mobileOpen: !this.state.mobileOpen });
   };
   headerColorChange = () => {
-    const { classes, color, changeColorOnScroll } = this.props;
     const windowsScrollTop = window.pageYOffset;
-    if (windowsScrollTop > changeColorOnScroll.height) {
-      document.body
-        .getElementsByTagName("header")[0]
-        .classList.remove(classes[color]);
-      document.body
-        .getElementsByTagName("header")[0]
-        .classList.add(classes[changeColorOnScroll.color]);
-      document
-        .getElementById("profile-name")
-        .classList.add(classes[changeColorOnScroll.color]);
+    if (windowsScrollTop > 600) {
+      this.refs["headerNavBar-ref"].classList.add("scrolledHeader-style");
+      this.refs["headerNavBar-ref"].classList.remove("transparentHeader-style");
     } else {
-      document.body
-        .getElementsByTagName("header")[0]
-        .classList.add(classes[color]);
-      document.body
-        .getElementsByTagName("header")[0]
-        .classList.remove(classes[changeColorOnScroll.color]);
-      document
-        .getElementById("profile-name")
-        .classList.remove(classes[changeColorOnScroll.color]);
+      this.refs["headerNavBar-ref"].classList.remove("scrolledHeader-style");
+      this.refs["headerNavBar-ref"].classList.add("transparentHeader-style");
     }
   };
   componentDidMount() {
-    if (this.props.changeColorOnScroll) {
-      window.addEventListener("scroll", this.headerColorChange);
-    }
+    this.refs["headerNavBar-ref"].classList.add("header-initialStyles")
+    window.addEventListener("scroll", this.headerColorChange);
   }
   componentWillUnmount() {
-    if (this.props.changeColorOnScroll) {
-      window.removeEventListener("scroll", this.headerColorChange);
-    }
+    window.removeEventListener("scroll", this.headerColorChange);
   }
   render() {
-    const {
-      classes,
-      color,
-      rightLinks,
-      leftLinks,
-      profileName,
-      fixed,
-      absolute
-    } = this.props;
-    const appBarClasses = classNames({
-      [classes.appBar]: true,
-      [classes[color]]: color,
-      [classes.absolute]: absolute,
-      [classes.fixed]: fixed
-    });
-    const profileNameButton = (
-      <Button id="profile-name" className={classes.title + " profile-name"} href="#landingPage-section">
-        {profileName}
-      </Button>
-    );
     return (
-      <div className="header-container">
-        <AppBar className={appBarClasses}>
-          <Toolbar className={classes.container}>
-            {leftLinks !== undefined ? profileName : null}
-            <div className={classes.flex}>
-              {leftLinks !== undefined ? (
-                <Hidden smDown implementation="css">
-                  {leftLinks}
-                </Hidden>
-              ) : (
-                profileNameButton
-              )}
-            </div>
-            <Hidden smDown implementation="css">
-              {rightLinks}
-            </Hidden>
-            <Hidden mdUp>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={this.handleDrawerToggle}
-              >
-                <Menu />
-              </IconButton>
-            </Hidden>
-          </Toolbar>
-          <Hidden mdUp implementation="js">
-            <Drawer
-              variant="temporary"
-              anchor={"right"}
-              open={this.state.mobileOpen}
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              onClose={this.handleDrawerToggle}
-            >
-              <div className={classes.appResponsive}>
-                {leftLinks}
-                {rightLinks}
-              </div>
-            </Drawer>
-          </Hidden>
-        </AppBar>
+      <div
+        id="headerNavBar"
+        className="header-container row no-gutters w-100 header-commonStyles"
+        ref="headerNavBar-ref"
+      >
+        <div className="col-12">
+          <HeaderLinks />
+        </div>
       </div>
     );
   }
@@ -137,43 +65,43 @@ Header.defaultProp = {
 };
 
 Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  color: PropTypes.oneOf([
-    "primary",
-    "info",
-    "success",
-    "warning",
-    "danger",
-    "transparent",
-    "white",
-    "rose",
-    "dark"
-  ]),
-  rightLinks: PropTypes.node,
-  leftLinks: PropTypes.node,
-  profileName: PropTypes.string,
-  fixed: PropTypes.bool,
-  absolute: PropTypes.bool,
-  // this will cause the sidebar to change the color from
-  // this.props.color (see above) to changeColorOnScroll.color
-  // when the window.pageYOffset is heigher or equal to
-  // changeColorOnScroll.height and then when it is smaller than
-  // changeColorOnScroll.height change it back to
-  // this.props.color (see above)
-  changeColorOnScroll: PropTypes.shape({
-    height: PropTypes.number.isRequired,
-    color: PropTypes.oneOf([
-      "primary",
-      "info",
-      "success",
-      "warning",
-      "danger",
-      "transparent",
-      "white",
-      "rose",
-      "dark"
-    ]).isRequired
-  })
+  // classes: PropTypes.object.isRequired,
+  // color: PropTypes.oneOf([
+  //   "primary",
+  //   "info",
+  //   "success",
+  //   "warning",
+  //   "danger",
+  //   "transparent",
+  //   "white",
+  //   "rose",
+  //   "dark"
+  // ]),
+  // rightLinks: PropTypes.node,
+  // leftLinks: PropTypes.node,
+  // profileName: PropTypes.string,
+  // fixed: PropTypes.bool,
+  // absolute: PropTypes.bool
+  // // this will cause the sidebar to change the color from
+  // // this.props.color (see above) to changeColorOnScroll.color
+  // // when the window.pageYOffset is heigher or equal to
+  // // changeColorOnScroll.height and then when it is smaller than
+  // // changeColorOnScroll.height change it back to
+  // // this.props.color (see above)
+  // // changeColorOnScroll: PropTypes.shape({
+  // //   height: PropTypes.number.isRequired,
+  // //   color: PropTypes.oneOf([
+  // //     "primary",
+  // //     "info",
+  // //     "success",
+  // //     "warning",
+  // //     "danger",
+  // //     "transparent",
+  // //     "white",
+  // //     "rose",
+  // //     "dark"
+  // //   ]).isRequired
+  // // })
 };
 
 export default withStyles(headerStyle)(Header);
