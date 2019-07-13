a242a3340c53734c9c6130f0c8ea2b0c613e655b
ravibp <ravi.prasanna@infosys.com>
2019-07-14 01:14:33 +0530

    ScrollSpy, Homepage BG Video, Skills Section done

--- src/myComponents/Header/HeaderLinks.jsx
+++ src/myComponents/Header/HeaderLinks.jsx
@@ -21,65 +21,32 @@ import Button from "components/CustomButtons/Button.jsx";
 import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
 import Scrollspy from "react-scrollspy";
 
-// let headerLinkBS = (
-//   <nav id="mainNavBar" class="navbar navbar-inverse navbar-fixed-top">
-//     <div class="container">
-//       <div class="navbar-header">
-//         <button
-//           type="button"
-//           class="navbar-toggle"
-//           data-toggle="collapse"
-//           data-target=".navbar-collapse"
-//         >
-//           <span class="icon-bar"></span>
-//           <span class="icon-bar"></span>
-//           <span class="icon-bar"></span>
-//         </button>
-//       </div>
-//       <div class="navbar-collapse collapse">
-//         <ul class="nav navbar-nav">
-//           <li class="active">
-//             <a href="#aboutMe-section">Desert</a>
-//           </li>
-//           <li>
-//             <a href="#divLighthouse">Lighthouse</a>
-//           </li>
-//           <li>
-//             <a href="#divTulips">Tulips</a>
-//           </li>
-//           <li class="dropdown">
-//             <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-//               Animals <span class="caret"></span>
-//             </a>
-//             <ul class="dropdown-menu">
-//               <li>
-//                 <a href="#divJellyfish">Jellyfish</a>
-//               </li>
-//               <li>
-//                 <a href="#divPenguins">Penguins</a>
-//               </li>
-//             </ul>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   </nav>
-// );
 function HeaderLinks({ ...props }) {
   const { classes } = props;
   return (
     <div className="headerLinks">
       <div className="row">
-        <div className="col-6 headerLinks__profile-name" >
+        <div className="col-6 headerLinks__profile-name">
+        <Scrollspy
+            items={[
+              "landingPage-section",
+            ]}
+          >
           <li>
-            <a href="#landingPage-section" >Ravi BP</a>
+            <a href="#landingPage-section">Ravi BP</a>
           </li>
+          </Scrollspy>
         </div>
+
         <div className="col-6 headerLinks__rightLinks">
           <Scrollspy
-            items={["aboutMe-section", "skills-section", "projects-section"]}
+            items={[
+              "aboutMe-section",
+              "skills-section",
+              "projects-section"
+            ]}
             currentClassName="is-current"
-            offset={ -70 }
+            offset={-10}
           >
             <li>
               <a href="#aboutMe-section">About</a>
