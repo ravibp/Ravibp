38ddc11d63d4fddeb82d2caf6b1ebed5926e37b4
Ravi BP <ravi.prasanna@infosys.com>
2019-09-01 02:44:36 +0530

    V.1.6 projects section (#10)
    * projects completed for desktop

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.7.0";
 
-// pages for this product
-import Components from "views/Components/Components.jsx";
-import LandingPage from "views/LandingPage/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
+// route pages
+import LandingPage from "myComponents/LandingPage.jsx";
+import ResumeBuild from "myComponents/ResumeBuild.jsx";
 
-var hist = createBrowserHistory();
+  ReactDOM.render(
+    <BrowserRouter
+      basename={process.env.PUBLIC_URL}
+    >
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/resume" component={ResumeBuild} />
+      </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
 
-ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={Components} />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
